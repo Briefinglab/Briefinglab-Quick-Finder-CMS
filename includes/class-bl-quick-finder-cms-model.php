813897d41bfc02bffac6d19c7b0d9f3c10893dca
Luca Maroni <lcmaroni77@gmail.com>
2015-03-12 14:15:10 +0100

    manage menu_order for quick-finders objects

--- includes/class-bl-quick-finder-cms-model.php
+++ includes/class-bl-quick-finder-cms-model.php
@@ -17,7 +17,9 @@ class Bl_Quick_Finder_Cms_Model {
 
         $args = array(
             'post_type' => 'bl-quick-finder',
-            'posts_per_page' => $atts['limit']
+            'posts_per_page' => $atts['limit'],
+            'orderby' => 'menu_order',
+            'order' => 'ASC'
         );
 
         if( ! empty ( $atts['categories'] ) ){
